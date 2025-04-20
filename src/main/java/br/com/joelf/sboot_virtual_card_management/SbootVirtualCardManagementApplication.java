8773dd1e3d21a988681d3053e1021f2be605e9ae
Joel da Silva Pereira Filho <joelfilho02812@gmail.com>
2025-04-20 17:32:43 -0300

    chore: init project

---
+++ src/main/java/br/com/joelf/sboot_virtual_card_management/SbootVirtualCardManagementApplication.java
@@ -0,0 +1,13 @@
+package br.com.joelf.sboot_virtual_card_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbootVirtualCardManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbootVirtualCardManagementApplication.class, args);
+    }
+
+}
