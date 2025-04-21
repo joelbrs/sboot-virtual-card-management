dc89213e02db84de62cf774c1c43997cc9a91276
Joel da Silva Pereira Filho <joelfilho02812@gmail.com>
2025-04-20 21:30:02 -0300

    feat: ensure user has a random credit limit

--- src/main/java/br/com/joelf/sboot_virtual_card_management/users/domain/service/UserService.java
+++ src/main/java/br/com/joelf/sboot_virtual_card_management/users/domain/service/UserService.java
@@ -4,6 +4,8 @@ import br.com.joelf.sboot_virtual_card_management.users.domain.entities.User;
 import br.com.joelf.sboot_virtual_card_management.users.domain.port.UserRepository;
 import lombok.RequiredArgsConstructor;
 
+import java.math.BigDecimal;
+import java.util.Random;
 import java.util.UUID;
 
 @RequiredArgsConstructor
@@ -11,7 +13,15 @@ public class UserService {
 
     private final UserRepository userRepository;
 
+    private final BigDecimal minCreditLimit;
+    private final BigDecimal maxCreditLimit;
+
     public UUID create(User user) {
+        Random random = new Random();
+
+        BigDecimal creditLimit =
+                minCreditLimit.add(BigDecimal.valueOf(random.nextDouble() * maxCreditLimit.subtract(minCreditLimit).doubleValue()));
+        user.setCreditLimit(creditLimit);
         return userRepository.create(user);
     }
 }
